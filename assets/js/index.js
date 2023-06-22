86baed4d0d7b98ee91dcdece85127c6f1d2c56a8
Gab13y <gabby.peneyra@gmail.com>
2023-06-22 14:52:32 +0800

    new additional

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,8 @@
 var greeting_messages=["Hello", "Welcome"];
 var lastLogin = '';
 var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'];
+var currentPassword = 'GarabileS0813!!';
+var savedSessionPassword = ''
 $(document).ready(function(){
     var slInterval = setInterval(() => {
         $("body").css("background-color", "#fff");
@@ -8,15 +10,41 @@ $(document).ready(function(){
         clearInterval(slInterval);
         redirectToDashboard1();
     }, 500);
+    $('.my-account').on('click',function(){
+        if(savedSessionPassword === ''){
+            loginPassword1();
+        }
+    });
+    $('#close-login-password').on('click', function(){
+        $('#screen1').removeClass('img-hide');
+        $('#login-password1').addClass('img-hide');
+    });
+    $('#back-login-password2').on('click', function(){
+        $('#login-password2').addClass('img-hide');
+        $('#login-password1').removeClass('img-hide');
+    });
+    $('#close-login-password2').on('click', function(){
+        $('#screen1').removeClass('img-hide');
+        $('#login-password2').addClass('img-hide');
+    });
+    $('#password-login-password').on('click', function(){
+        $('#login-password1').addClass('img-hide');
+        $('#login-password2').removeClass('img-hide');
+        $('#button-login-password').prop('disabled', true);
+    });
 });
+function loginPassword1(){    
+    $('#screen1').addClass('img-hide');
+    $('#login-password1').removeClass('img-hide');
+}
 function redirectToDashboard1(){
     var bgInterval = setInterval(() => {
         $('#bg').remove();
         $('#screen1').removeClass('img-hide');
         clearInterval(bgInterval);
-        $('#welcome_greeting label').append(getRandomGreetingMessage() + ', Gabby');
+        //$('#welcome_greeting label').append(getRandomGreetingMessage() + ', Gabby');
         lastLogin = getLastLogin();
-        $('#welcome_greeting span').append("Your last login was " + lastLogin);
+        //$('#welcome_greeting span').append("Your last login was " + lastLogin);
         $("body").css("background-color", "#f3f4f9");
     }, 2000);
 }
@@ -35,3 +63,13 @@ function getLastLogin() {
     var _date = dt.getDate() + " "  + months[dt.getMonth()] + " "  + dt.getFullYear();
     return _date + ' ' + time;
 } 
+function validatePassword(obj){
+    if($(obj).val() !== ''){
+        $('#button-login-password').prop('disabled', false);
+        $('#button-login-password').css('background-color','rgb(7 34 105)');
+        $('#button-login-password').css('color','#fff');
+    } else{
+        $('#button-login-password').css('background-color','#e6e6e6');
+        $('#button-login-password').css('color','#9c9c9c');
+    }
+}
